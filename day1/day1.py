313d0fc3cedadbdc74cee5e036ad7231ee3ea2e3
Andrew Richter <drew@Andrews-MBP.ht.home>
2023-12-01 22:25:38 -0600

    wip

---
+++ day1/day1.py
@@ -0,0 +1,10 @@
+import urllib.request; 
+
+
+def solve_one(problem = String):
+	s = urllib.request.urlretrieve(problem, "problem1.txt")
+	
+
+
+
+solve_one(https://adventofcode.com/2023/day/1/input)
